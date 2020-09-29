3ec15b5230ddb71b329c66db1c6f1b49b70b4951
陈杨君 <chenyj101@taikanglife.com>
2020-09-29 17:55:00 +0800

    提交VUE项目

---
+++ pic_under_writing/src/components/PicUW.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="PicUW">
+    <div class="container">
+      <div class="row clearfix">
+        <div class="col-md-12 column">
+          <div class="row clearfix">
+            <div class="col-md-6 column">
+              <h1>左边</h1>
+            </div>
+            <div class="col-md-6 column">
+              <h1>右边</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PicUW',
+  data () {
+    return {
+      author: '微信公众号 jinkey-love'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
